3529c9c862908b4bfe1736c8fa396b12778d680d
MihoZaki <mihoubi.zakaria.dev@gmail.com>
2026-04-15 10:54:54 +0100

    lexer: add tokenization for array literals

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -53,6 +53,10 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.LBRACE, l.ch)
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
+	case '[':
+		tok = newToken(token.LBRACKET, l.ch)
+	case ']':
+		tok = newToken(token.RBRACKET, l.ch)
 	case '+':
 		if l.peekChar() == '=' {
 			tok = l.makeTwoCharToken(l.ch, '=', token.PLUS, token.PLUS_ASSIGN)
