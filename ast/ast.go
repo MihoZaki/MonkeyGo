f5833d659e166b06368cf58e2c1a25a299bd4615
MihoZaki <mihoubi.zakaria.dev@gmail.com>
2026-05-07 16:37:00 +0100

    fix: typo in BlockStatement method name

--- ast/ast.go
+++ ast/ast.go
@@ -203,7 +203,7 @@ type BlockStatement struct {
 	Statements []Statement
 }
 
-func (bs *BlockStatement) statement()           {}
+func (bs *BlockStatement) statementNode()       {}
 func (bs *BlockStatement) TokenLiteral() string { return bs.Token.Literal }
 func (bs *BlockStatement) String() string {
 	var out strings.Builder
